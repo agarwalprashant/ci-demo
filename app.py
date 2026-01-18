5e5718ce1ce756e96fea72e68db22ffcfd42423e
Prashant Agarwal <agarwalprashant355@gmail.com>
2026-01-18 20:24:41 +0530

    feat: Add unit tests for math functions, integrate testing into the CI workflow across multiple environments, and refactor `app.py` by renaming `multiply` to `mul` and encapsulating example usage.

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ def sub(a, b):
     return a - b
 
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
 
-result1 = add(10, 20)
-result2 = sub(20, 10)
-result3 = multiply(10, 20)
+if __name__ == "__main__":  
+    result1 = add(10, 20)
+    result2 = sub(20, 10)
+    result3 = mul(10, 20)
 
-print("Addition Result:", result1)
-print("Subtraction Result:", result2)
-print("Multiplication Result:", result3)
+    print("Addition Result:", result1)
+    print("Subtraction Result:", result2)
+    print("Multiplication Result:", result3)
