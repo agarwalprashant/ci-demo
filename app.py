00cd1f38b5ea1975e669137d606ae31ca6d84164
Prashant Agarwal <agarwalprashant355@gmail.com>
2026-01-18 19:53:52 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+def add(a,b):
+    return a + b
+
+
+
+
+
+
+
+
+
+
+
+def sub(a, b):return a-b
+
+result1 = add(10, 20)
+result2 = sub(20, 10)
+
+print("Addition Result:", result1)
+print("Subtraction Result:", result2)
