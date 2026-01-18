8572ef7c715bb35eeb09505cc45a996aeb93fa3e
Prashant Agarwal <agarwalprashant355@gmail.com>
2026-01-18 20:16:24 +0530

    feat: add multiplication function and display its result.

--- app.py
+++ app.py
@@ -6,8 +6,14 @@ def sub(a, b):
     return a - b
 
 
+def multiply(a, b):
+    return a * b
+
+
 result1 = add(10, 20)
 result2 = sub(20, 10)
+result3 = multiply(10, 20)
 
 print("Addition Result:", result1)
 print("Subtraction Result:", result2)
+print("Multiplication Result:", result3)
