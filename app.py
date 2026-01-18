555894a9b13bcce83eb8646174f19bd741cbf231
Prashant Agarwal <agarwalprashant355@gmail.com>
2026-01-18 20:29:23 +0530

    feat: Add multiplication function, implement unit tests, and integrate a multi-platform testing job into the CI workflow.

--- app.py
+++ app.py
@@ -6,8 +6,16 @@ def sub(a, b):
     return a - b
 
 
-result1 = add(10, 20)
-result2 = sub(20, 10)
+def mul(a, b):
+    return a * b
 
-print("Addition Result:", result1)
-print("Subtraction Result:", result2)
+
+if __name__ == "__main__":  
+    result1 = add(10, 20)
+    result2 = sub(20, 10)
+    result3 = mul(10, 20)
+
+    print("Addition Result:", result1)
+    print("Subtraction Result:", result2)
+    print("Multiplication Result:", result3)
+
